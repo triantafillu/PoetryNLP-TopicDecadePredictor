7273ad857430bfd07872dab9aa8d16b622e10f60
Aleksandra Triantafillu <alexandra00219@gmail.com>
2021-07-12 20:37:59 +0300

    updated deploy

--- deploy/aleksandra/app.py
+++ deploy/aleksandra/app.py
@@ -1,5 +1,8 @@
+import json
+
 import numpy as np
 from flask import Flask, request, jsonify, render_template
+from keras_preprocessing.text import tokenizer_from_json
 from tensorflow import keras
 from utils import convert_to_decade, preprocess_input, themes_prediction
 import pickle
@@ -7,23 +10,26 @@ import pickle
 app = Flask(__name__)
 model1 = keras.models.load_model('year_prediction.h5')
 with open('tokenizer1.pickle', 'rb') as handle:
-    tokenizer = pickle.load(handle)
+    tokenizer1 = pickle.load(handle)
+with open('tokenizer2.json') as handle2:
+    data = json.load(handle2)
+    tokenizer2 = tokenizer_from_json(data)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict',methods=['POST', 'GET'])
 def predict():
-    form = ''
-    for x in request.form.values():
-        form.join(x)
-    input = preprocess_input(form, tokenizer)
+    title = request.form['title']
+    text = request.form['text']
+    form = title + ' ' + text
+    input = preprocess_input(form, tokenizer1)
     prediction = model1.predict(input)
     output1 = convert_to_decade(prediction[0][0])
-    output2 = ', '.join(themes_prediction(form, tokenizer))
+    output2 = ', '.join(themes_prediction(form, tokenizer2))
 
-    return render_template('index.html', prediction_text1=f"""This poem was written in {output1}""", prediction_text2 =f"The themes are: {output2}")
+    return render_template('index.html', prediction_text1=f"""This poem was written in {int(output1)}s""", prediction_text2 =f"The themes are: {output2}")
 
 
 if __name__ == "__main__":
--- deploy/aleksandra/utils.py
+++ deploy/aleksandra/utils.py
@@ -80,26 +80,3 @@ def themes_prediction(text, tokenizer):
     result.append('living')
 
   return result
-
-t = """So now I have confessed that he is thine,
-And I my self am mortgaged to thy will,
-Myself I’ll forfeit, so that other mine
-Thou wilt restore to be my comfort still:
-But thou wilt not, nor he will not be free,
-For thou art covetous, and he is kind;
-He learned but surety-like to write for me,
-Under that bond that him as fast doth bind.
-The statute of thy beauty thou wilt take,
-Thou usurer, that put’st forth all to use,
-And sue a friend came debtor for my sake;
-So him I lose through my unkind abuse.
-    Him have I lost; thou hast both him and me:
-    He pays the whole, and yet am I not free.
-"""
-
-with open('tokenizer1.pickle', 'rb') as handle:
-  tokenizer = pickle.load(handle)
-
-model1 = keras.models.load_model('year_prediction.h5')
-
-print(model1.predict(preprocess_input(t,tokenizer)))
