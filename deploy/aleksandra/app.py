989dd05cb2a4d941901a0dc2cae73b0adaa59df4
Aleksandra Triantafillu <alexandra00219@gmail.com>
2021-07-12 19:18:29 +0300

    updated for themes

--- deploy/aleksandra/app.py
+++ deploy/aleksandra/app.py
@@ -1,32 +1,29 @@
 import numpy as np
 from flask import Flask, request, jsonify, render_template
 from tensorflow import keras
-from utils import convert_to_decade, preprocess_input
+from utils import convert_to_decade, preprocess_input, themes_prediction
 import pickle
 
 app = Flask(__name__)
-model = keras.models.load_model('year_prediction.h5')
-with open('tokenizer.pickle', 'rb') as handle:
+model1 = keras.models.load_model('year_prediction.h5')
+with open('tokenizer1.pickle', 'rb') as handle:
     tokenizer = pickle.load(handle)
 
 @app.route('/')
 def home():
-    return render_template('../Bootcamp-Repository-Language-2/deploy/aleksandra/templates/index.html')
+    return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
     form = ''
     for x in request.form.values():
         form.join(x)
     input = preprocess_input(form, tokenizer)
-    prediction = model.predict(input)
+    prediction = model1.predict(input)
+    output1 = convert_to_decade(prediction[0][0])
+    output2 = ', '.join(themes_prediction(form, tokenizer))
 
-    output = convert_to_decade(prediction[0])
-
-    return render_template('../Bootcamp-Repository-Language-2/deploy/aleksandra/templates/index.html', prediction_text=f"""This poem was written in {int(output[0])}s\nThe themes are: """)
+    return render_template('index.html', prediction_text1=f"""This poem was written in {output1}""", prediction_text2 =f"The themes are: {output2}")
 
 
 if __name__ == "__main__":
--- deploy/aleksandra/utils.py
+++ deploy/aleksandra/utils.py
@@ -1,12 +1,21 @@
+import pickle
+
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import nltk
 nltk.download('wordnet')
 from nltk.stem import WordNetLemmatizer
+from tensorflow import keras
 nltk.download('stopwords')
 import numpy as np
 
+theme_models = [keras.models.load_model('baby_models/model_animals.h5'),
+                keras.models.load_model('baby_models/model_body.h5'),
+                keras.models.load_model('baby_models/model_family.h5'),
+                keras.models.load_model('baby_models/model_love.h5'),
+                keras.models.load_model('baby_models/model_nature.h5')]
+theme_labels = ['animals', 'body', 'family', 'love', 'nature']
 
 
 def full_form(word):
@@ -51,3 +60,46 @@ def convert_to_decade(x):
   res = dec * 10
   return res
 
+def themes_prediction(text, tokenizer):
+  global theme_models, theme_labels
+  input = preprocess_input(text,tokenizer)
+  scores = []
+  for model in theme_models:
+    res = model.predict(input)
+    if res[0][0] >= 0.3:
+      scores.append(1)
+    else:
+      scores.append(0)
+
+  result = []
+  for i in range(len(scores)):
+    if scores[i] == 1:
+      result.append(theme_labels[i])
+
+  if len(result) == 0:
+    result.append('living')
+
+  return result
+
+t = """So now I have confessed that he is thine,
+And I my self am mortgaged to thy will,
+Myself I’ll forfeit, so that other mine
+Thou wilt restore to be my comfort still:
+But thou wilt not, nor he will not be free,
+For thou art covetous, and he is kind;
+He learned but surety-like to write for me,
+Under that bond that him as fast doth bind.
+The statute of thy beauty thou wilt take,
+Thou usurer, that put’st forth all to use,
+And sue a friend came debtor for my sake;
+So him I lose through my unkind abuse.
+    Him have I lost; thou hast both him and me:
+    He pays the whole, and yet am I not free.
+"""
+
+with open('tokenizer1.pickle', 'rb') as handle:
+  tokenizer = pickle.load(handle)
+
+model1 = keras.models.load_model('year_prediction.h5')
+
+print(model1.predict(preprocess_input(t,tokenizer)))
